99dc9185c2ba8c24099bb94e44839ecc52eeabb2
Rachaeldagge <51501849+Rachaeldagge@users.noreply.github.com>
2020-09-26 10:09:36 +1000

    update

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -137,5 +137,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-db_from_env = dj_datatbase_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
